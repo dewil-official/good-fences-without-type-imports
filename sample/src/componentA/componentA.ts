749830b3d4766422021c322c467f7d5d707aa214
Dennis W <7500200+dewil-official@users.noreply.github.com>
2023-11-29 15:46:24 +0100

    Updated sample to include type import

--- sample/src/componentA/componentA.ts
+++ sample/src/componentA/componentA.ts
@@ -1,6 +1,9 @@
 import componentB from '../componentB/componentB';
 import helperA1 from './helperA1';
 import helperA2 from './helperA2';
+import { JustAType } from '../just-types/types.i';
+
+export const exampleObjectWithType: JustAType = { info: '' };
 
 export default function componentA() {
     componentB();
---
+++ sample/src/just-types/types.i.ts
@@ -0,0 +1,3 @@
+export type JustAType = {
+    info: string;
+};
